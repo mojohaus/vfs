fd87dc4997c90aca0d64a451e740b49cf2456410
Dan Tran <dantran@gmail.com>
2012-05-24 03:52:48 +0000

    initial VfsFileSetManage implementation
    git-svn-id: file:///home/tiste/MOJOHAUS-TO-GIT/SVN-MOJO-WIP/trunk/sandbox/vfs@16701 52ab4f32-60fc-0310-b215-8acea882cd1b

--- vfs-utils/src/main/java/org/codehaus/mojo/vfs/internal/DefaultVfsFileSetManager.java
+++ vfs-utils/src/main/java/org/codehaus/mojo/vfs/internal/DefaultVfsFileSetManager.java
@@ -26,21 +26,55 @@ public class DefaultVfsFileSetManager
     public void delete( VfsFileSet fileSet )
         throws FileSystemException
     {
-        // TODO Auto-generated method stub
+        VfsDirectoryScanner scanner = new DefaultVfsDirectoryScanner();
+        scanner.setIncludes( fileSet.getIncludes() );
+        scanner.setExcludes( fileSet.getExcludes() );
+        scanner.setStartingDirectory( fileSet.getDirectory() );
+        scanner.setCaseSensitive( fileSet.isCaseSensitive() );
+        List<FileObject> fos = scanner.scan();
+        for ( FileObject fo : fos )
+        {
+            //TODO check for return error
+            fo.delete();
+        }
 
     }
 
     public void move( VfsFileSet fileSet )
         throws FileSystemException
     {
-        // TODO Auto-generated method stub
+        VfsDirectoryScanner scanner = new DefaultVfsDirectoryScanner();
+        scanner.setIncludes( fileSet.getIncludes() );
+        scanner.setExcludes( fileSet.getExcludes() );
+        scanner.setStartingDirectory( fileSet.getDirectory() );
+        scanner.setCaseSensitive( fileSet.isCaseSensitive() );
+        List<FileObject> fos = scanner.scan();
+        for ( FileObject fo : fos )
+        {
+            //        
+        }
+
+        for ( FileObject fo : fos )
+        {
+            fo.delete();
+        }
 
     }
 
     public void copy( VfsFileSet fileSet )
         throws FileSystemException
     {
-        // TODO Auto-generated method stub
+        VfsDirectoryScanner scanner = new DefaultVfsDirectoryScanner();
+        scanner.setIncludes( fileSet.getIncludes() );
+        scanner.setExcludes( fileSet.getExcludes() );
+        scanner.setStartingDirectory( fileSet.getDirectory() );
+        scanner.setCaseSensitive( fileSet.isCaseSensitive() );
+        List<FileObject> fos = scanner.scan();
+        for ( FileObject fo : fos )
+        {
+            //TODO check for return error
+            // copy from src to dest
+        }
 
     }
 
