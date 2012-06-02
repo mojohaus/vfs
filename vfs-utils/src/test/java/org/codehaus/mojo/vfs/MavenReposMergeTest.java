bb4ed5e446b289492beb88a1545a6eaadeddcea8
Dan Tran <dantran@gmail.com>
2012-06-02 22:03:11 +0000

    Initial repo merge test setup
    git-svn-id: file:///home/tiste/MOJOHAUS-TO-GIT/SVN-MOJO-WIP/trunk/sandbox/vfs@16775 52ab4f32-60fc-0310-b215-8acea882cd1b

--- vfs-utils/src/test/java/org/codehaus/mojo/vfs/MavenReposMergeTest.java
+++ vfs-utils/src/test/java/org/codehaus/mojo/vfs/MavenReposMergeTest.java
@@ -4,6 +4,7 @@ import java.io.File;
 
 import org.apache.commons.vfs2.FileObject;
 import org.apache.commons.vfs2.VFS;
+import org.codehaus.mojo.vfs.internal.DefaultMergeVfsMavenRepositories;
 import org.codehaus.mojo.vfs.internal.DefaultVfsFileSetManager;
 import org.codehaus.plexus.util.FileUtils;
 import org.junit.Test;
@@ -13,41 +14,50 @@ public class MavenReposMergeTest
 {
     private VfsFileSetManager filesetManager = new DefaultVfsFileSetManager();
 
+    private MergeVfsMavenRepositories repoMerger = new DefaultMergeVfsMavenRepositories();
+    
     @Test
     public void testMavenReposMerge()
         throws Exception
     {
 
+        basedir = basedir.getAbsoluteFile();
+        builddir = builddir.getAbsoluteFile();
+
         File testDirectory = new File( builddir, "test-repos" );
         FileUtils.deleteDirectory( testDirectory );
 
         FileObject scmSourceRepo = VFS.getManager()
-            .resolveFile( "file://" + new File( basedir.getAbsoluteFile(), "src/test/data/repos/source" ) );
+            .resolveFile( "file://" + new File( basedir, "src/test/data/repos/source" ) );
 
         FileObject scmTargetRepo = VFS.getManager()
-            .resolveFile( "file://" + new File( basedir.getAbsoluteFile(), "src/test/data/repos/target" ) );
+            .resolveFile( "file://" + new File( basedir, "src/test/data/repos/target" ) );
 
         FileObject testSourceRepo = VFS.getManager()
-            .resolveFile( "file://" + new File( builddir.getAbsoluteFile(), "test-repos/source" ) );
+            .resolveFile( "file://" + new File( builddir, "test-repos/source" ) );
 
         FileObject testTargetRepo = VFS.getManager()
-            .resolveFile( "file://" + new File( builddir.getAbsoluteFile(), "test-repos/target" ) );
+            .resolveFile( "file://" + new File( builddir, "test-repos/target" ) );
 
         VfsFileSet fileset = new VfsFileSet();
         String[] allFiles = { "**" };
         fileset.setIncludes( allFiles );
-        String[] hiddenFiles = { "**/.*/**" };
+        String[] hiddenFiles = { "**/.*/**" }; //dont want local svn metadata file sneaking in
         fileset.setExcludes( hiddenFiles );
 
         //prep source repo
         fileset.setSource( scmSourceRepo );
         fileset.setDestination( testSourceRepo );
         filesetManager.copy( fileset );
-        
+
         //prep target repo
         fileset.setSource( scmTargetRepo );
         fileset.setDestination( testTargetRepo );
         filesetManager.copy( fileset );
+        
+        File stagingDir = new File( builddir, "test-repos/staging" );
+        
+        repoMerger.merge( testSourceRepo, testTargetRepo, stagingDir, true );
 
     }
 }
