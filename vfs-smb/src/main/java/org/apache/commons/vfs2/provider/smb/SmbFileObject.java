3b05ef9d7847c2e47a707cf80a46759e23f2f779
Dan Tran <dan.tran@emc.com>
2017-01-22 01:56:03 -0800

    Upgrade commons-vfs2 to 2.1, deploy vfs-smb provider
    This plugin now requires
    
     * maven 3.x
     * java 7
     
    Other upgrades
    
     * jcifs 1.3.7
     * plexus-utils 3.0.24
     * commons-net 3.5
     
    Others
    
     * Revamp dependencyManagement

--- vfs-smb/src/main/java/org/apache/commons/vfs2/provider/smb/SmbFileObject.java
+++ vfs-smb/src/main/java/org/apache/commons/vfs2/provider/smb/SmbFileObject.java
@@ -4,9 +4,9 @@
  * copyright ownership. The ASF licenses this file to You under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance with the License. You may obtain a
  * copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License
  * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
  * or implied. See the License for the specific language governing permissions and limitations under
@@ -17,12 +17,6 @@ package org.apache.commons.vfs2.provider.smb;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.MalformedURLException;
-
-import jcifs.smb.NtlmPasswordAuthentication;
-import jcifs.smb.SmbException;
-import jcifs.smb.SmbFile;
-import jcifs.smb.SmbFileInputStream;
-import jcifs.smb.SmbFileOutputStream;
 
 import org.apache.commons.vfs2.FileName;
 import org.apache.commons.vfs2.FileObject;
@@ -37,12 +31,17 @@ import org.apache.commons.vfs2.provider.UriParser;
 import org.apache.commons.vfs2.util.RandomAccessMode;
 import org.apache.commons.vfs2.util.UserAuthenticatorUtils;
 
+import jcifs.smb.NtlmPasswordAuthentication;
+import jcifs.smb.SmbException;
+import jcifs.smb.SmbFile;
+import jcifs.smb.SmbFileInputStream;
+import jcifs.smb.SmbFileOutputStream;
+
 /**
  * A file in an SMB file system.
  */
 public class SmbFileObject
     extends AbstractFileObject
-    implements FileObject
 {
     // private final String fileName;
     private SmbFile file;
